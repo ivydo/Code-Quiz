c6255b0ea5cb74d0b4490e44cd4fc41d1ebee974
Ivy Do <ivy.do2011@gmail.com>
2021-11-10 02:46:37 -0500

    made start-page hidden in js

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+function startQuizToggle() {
+    var start = document.querySelector("start-page");
+    if (start.style.display === "none") {
+        start.style.display = "block";
+    } else {
+        start.style.display = "none";
+    }
+    }
