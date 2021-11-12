17eff23641a6843e9cccf69cab3821654dd1a5cb
Ivy Do <ivy.do2011@gmail.com>
2021-11-11 20:10:42 -0500

    disappear questions/choices

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,30 @@
+function MyTimer(callback, val) {
+  val = val || 60;
+  var timer = setInterval(function () {
+    callback(val);
+    if (val-- <= 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
 function startQuizToggle() {
-    var start = document.getElementById("start-page");
-    if (start.style.display === "none") {
-        start.style.display = "block";
-    } else {
-        start.style.display = "none";
-    }
-    }
+  var start = document.getElementById("container");
+  if (start.style.display === "none") {
+    start.style.display = "block";
+  } else {
+    start.style.display = "none";
+  }
+
+  new MyTimer(function (val) {
+    var timerMsg = "00:" + (val >= 10 ? val : "0" + val);
+    document.getElementById("timer").textContent = timerMsg;
+  });
+
+  var start = document.getElementById("question-container");
+  if (start.style.display === "block") {
+    start.style.display = "none";
+  } else {
+    start.style.display = "block";
+  }
+}
